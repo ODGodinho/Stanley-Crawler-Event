d98cd2a60467e3924df87979abaeceb84eb349b8
Victor Godinho <dragonsgamers@hotmail.com.br>
2023-03-05 21:22:42 -0300

    feat: :sparkles: add baseComponent class

---
+++ src/Pages/Components/BaseComponentPage.ts
@@ -0,0 +1,11 @@
+import { BasePage } from "../BasePage";
+
+/**
+ * Component Base for pages
+ *
+ * @abstract
+ * @class BaseComponentPage
+ */
+export abstract class BaseComponentPage extends BasePage {
+
+}
