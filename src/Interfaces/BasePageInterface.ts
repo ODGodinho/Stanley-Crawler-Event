356991e0ae9aff5710767e14df14968306094a76
ODGodinho <victor.godinho@plataforma13.com.br>
2024-11-29 00:31:25 -0300

    feat(Container): :sparkles: add support fluentBind

--- src/Interfaces/BasePageInterface.ts
+++
@@ -1,5 +0,0 @@
-import { type HandlerInterface, type PageInterface } from "@odg/chemical-x";
-
-import { type PageClassEngine } from "../engine";
-
-export type BasePageInterface = new(page: PageClassEngine) => HandlerInterface | PageInterface;
